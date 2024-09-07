f1212367e5f38ab7a76381d1a3907918dc83e6c9
Bugra Caliskandinc <bugracaliskandinc@gmail.com>
2024-09-07 19:35:12 +0300

    Added colors

--- wordle.go
+++ wordle.go
@@ -5,9 +5,37 @@ import (
 	"math/rand/v2"
 )
 
+type Color struct {
+	Reset   string
+	Red     string
+	Green   string
+	Yellow  string
+	Blue    string
+	Magenta string
+	Cyan    string
+	Gray    string
+	White   string
+}
+
+func initColor() *Color {
+	var color Color
+	color.Reset = "\033[0m"
+	color.Red = "\033[31m"
+	color.Green = "\033[32m"
+	color.Yellow = "\033[33m"
+	color.Blue = "\033[34m"
+	color.Magenta = "\033[35m"
+	color.Cyan = "\033[36m"
+	color.Gray = "\033[37m"
+	color.White = "\033[97m"
+
+	return &color
+}
+
 var wordleDict = []string{"BUGRA", "NESLI"}
 
 func main() {
+	color := initColor()
 	randSelected := wordleDict[rand.IntN(len(wordleDict))]
 
 	var userInput string
@@ -30,7 +58,7 @@ func main() {
 
 		for index, _ := range randSelected {
 			if randSelected[index] == userInput[index] {
-				fmt.Print(string(randSelected[index]))
+				fmt.Print(color.Green + string(randSelected[index]) + color.Reset)
 			} else {
 				isCorrect = false
 				fmt.Print("_")
